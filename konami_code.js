6d924f8722804d90e972ee794c3c7812258ec900
Mark Lawrence <lawrence.ml@gmail.com>
2019-01-12 16:37:57 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,24 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  const body = document.querySelector('body');
+  let index = 0;
 
+  body.addEventListener('keydown', function onKeyDownHandler(e) {
+    const key = parseInt(e.which || e.location);
+
+    if (key === code[index]) {
+      index++
+
+      if (index === code.length) {
+        alert('Congratulations! Konami Code accepted.');
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
+
+init()
